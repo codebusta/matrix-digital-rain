93562b5dca6de89b0ba2389989d761ac0c09bac9
codebusta <f.otaboev@gmail.com>
2025-05-05 11:14:06 +0400

    Create matrix-digital-rain.plugin.zsh

---
+++ matrix-digital-rain.plugin.zsh
@@ -0,0 +1,2 @@
+#Matrix plugin idea was creating re-using GitHub repo by amstrad [oh-my-matrix](https://github.com/amstrad/oh-my-matrix/blob/master/oh-my-matrix.plugin.zsh)
+alias matrix="python $(dirname "$0")/matrix.py"
